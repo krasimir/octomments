de57f259e012cb6cadf8a68d823b160c4cac0ccc
Krasimir Tsonev <krasimir@outset.ws>
2020-01-28 18:31:49 +0200

    New release

--- scripts/bumping-versions.js
+++ scripts/bumping-versions.js
@@ -1,4 +1,11 @@
+const fs = require('fs');
+
 ['client', 'renderer', 'server'].forEach(pkg => {
   const file = __dirname + '/../' + pkg + '/package.json';
   const packageJSON = require(file);
+  const bits = packageJSON.version.split('.').map(n => Number(n));
+  bits[bits.length - 1] = bits[bits.length - 1] + 1;
+  packageJSON.version = bits.join('.');
+  fs.writeFileSync(file, JSON.stringify(packageJSON, null, 2));
+  console.log(`Version of ${packageJSON.name} bumped to ${packageJSON.version}`);
 })
